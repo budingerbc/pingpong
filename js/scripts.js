437038c2f403e8da2b86a17e94f0643121893daa
Ben Budinger <budingerbc@gmail.com>
2017-06-25 12:52:41 -0700

    Create bottom column for listing ping/pong results, insert list items for results

--- js/scripts.js
+++ js/scripts.js
@@ -26,15 +26,23 @@ $(document).ready(function() {
       }
     }
 
-    // gets the user input from the form field and converts it to a integer
+    // gets the user input from the form field and converts it to an integer
     var userInput = parseInt($("#userInput").val());
     var results = [];
 
+    // checks to make sure the value entered is greater than 0
+    if (userInput <= 0) {
+      alert("Please enter an integer greater than 0.");
+    }
+    // loops starting at one up to and including user input value
     for (var count = 1; count <= userInput; count++) {
 
+      // check if divisible by 15 first to account for the value being divisiblbe by both 3 and 5
+      // then check if divisible by 3 and 5 and list appropriate results using DOM manipulation
       if (divisorFifteen(count)) {
-        results.push('pingpong');
+        $("#bottomColumn ul").append('<li>pingpong</li>');
       } else if (divisorFive(count)) {
+        $("#bottomColumn ul").append('<li>pong</li>');
         results.push('pong');
       } else if (divisorThree(count)) {
         results.push('ping');
@@ -44,6 +52,6 @@ $(document).ready(function() {
     }
 
     alert(results);
-  event.preventDefault();
+    event.preventDefault();
   });
 });
